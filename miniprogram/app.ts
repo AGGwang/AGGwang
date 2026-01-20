c8146e6943985dbedb985df27bcf275f73c4a41b
AGGwang <Aret2Paul@outlook.com>
2026-01-20 14:50:31 +0800

    修改配置上线云开发环境

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -23,7 +23,7 @@ App<IAppOption>({
         //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
         //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
         //   如不填则使用默认环境（第一个创建的环境）
-        env: 'cloud1-0g2coyy919fac611',
+        env: 'hischool7in3-6g95n1i3f4479bdc',
         traceUser: true,
       })
     }
